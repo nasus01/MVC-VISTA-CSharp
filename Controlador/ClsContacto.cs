096c4e6034265255519703e8c12616c4bb30f966
cristian morales <christianjack01@hotmail.com>
2019-10-06 13:18:44 -0500

    programa de base de datos

---
+++ Controlador/ClsContacto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlador
+{
+    public class ClsContacto
+    {
+        public int opc { get; set; }
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+    }
+}
